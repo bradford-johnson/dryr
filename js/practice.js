6510ffa7bc6156653c84798db2e8095e551bb950
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-01-31 19:48:58 -0500

    cleaned up file

--- js/practice.js
+++ js/practice.js
@@ -5,13 +5,10 @@ const mockDataPath = "js/mock_data.json";
 try {
   const data = fs.readFileSync(mockDataPath, "utf-8");
 
-  // Parse the JSON string into a JavaScript array of objects
   const jsonData = JSON.parse(data);
 
-  // Assuming you want to iterate through the keys of the first item (index 0)
   const firstItem = jsonData[0];
 
-  // Iterate through the keys of the first item and print them
   for (const key in firstItem) {
     console.log(key);
   }
